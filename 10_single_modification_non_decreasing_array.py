1e915e7b1a53dbe58bd53f960819e00cfa9d897e
Evgeny Stankevich <stankevich.evg@gmail.com>
2020-07-14 22:49:42 +0200

    10. Non-decreasing Array with Single Modification

---
+++ 10_single_modification_non_decreasing_array.py
@@ -0,0 +1,28 @@
+# You are given an array of integers in an arbitrary order.
+# Return whether or not it is possible to make the array non-decreasing by modifying at most 1 element to any value.
+#
+# We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+#
+# Example:
+#
+# [13, 4, 7] should return true, since we can modify 13 to any value 4 or less, to make it non-decreasing.
+#
+# [13, 4, 1] however, should return false, since there is no way to modify just one element to make the array non-decreasing.
+#
+# Can you find a solution in O(n) time?
+
+
+def check(lst):
+    counter = 0
+    last_element = None
+    for element in lst:
+        if last_element is not None and element < last_element:
+            counter += 1
+        last_element = element
+    return counter <= 1
+
+
+print(check([13, 4, 7]))
+# True
+print(check([5, 1, 3, 2, 5]))
+# False
