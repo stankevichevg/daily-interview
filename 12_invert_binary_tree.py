a7d32297577620746eca7c1667d2ef8825981341
Evgeny Stankevich <stankevich.evg@gmail.com>
2020-07-17 00:19:17 +0200

    11. Invert a Binary Tree

---
+++ 12_invert_binary_tree.py
@@ -0,0 +1,55 @@
+# You are given the root of a binary tree.
+# Invert the binary tree in place.
+# That is, all left children should become right children, and all right children should become left children.
+#
+# Example:
+#
+#     a
+#    / \
+#   b   c
+#  / \  /
+# d   e f
+#
+# The inverted version of this tree is as follows:
+#
+#   a
+#  / \
+#  c  b
+#  \  / \
+#   f e  d
+
+
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print(self.value)
+    if self.left:
+        self.left.preorder()
+    if self.right:
+        self.right.preorder()
+
+def invert(node):
+  if node is not None:
+      temp = node.right
+      node.right = node.left
+      node.left = temp
+      invert(node.left)
+      invert(node.right)
+
+
+root = Node('a')
+root.left = Node('b')
+root.right = Node('c')
+root.left.left = Node('d')
+root.left.right = Node('e')
+root.right.left = Node('f')
+
+root.preorder()
+# a b d e c f
+print("\n")
+invert(root)
+root.preorder()
+# a c f b e d
